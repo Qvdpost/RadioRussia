e4776a2480d29ba65d83748f7a8c2576cf2c8c61
Wouter <wouter-vrielink@hotmail.com>
2020-01-09 10:43:16 +0100

    README update

--- main.py
+++ main.py
@@ -9,11 +9,13 @@ if __name__ == '__main__':
     # Create the transmitter cost schemes
     transmitters = transmitters.CostScheme('data/transmitters.csv')
 
+    # Perform algorithm that keeps reassigning values until the case is solvedd
     randomize.random_reassignment(test_graph, transmitters.get_scheme(1))
 
+    # Example on how to get the nodes that have neighbours with the same value and print them
     violating_nodes = test_graph.get_violations()
-    print(len(violating_nodes))
 
+    print(len(violating_nodes))
     print(test_graph.get_violations())
 
     vis.visualise(test_graph, "data/US/gz_2010_us_040_00_500k.json")
