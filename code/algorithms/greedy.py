e3038d4ad17562e35498cd0ad67f2060d3236328
Wouter <wouter-vrielink@hotmail.com>
2020-01-15 11:42:18 +0100

    Greedy update

--- code/algorithms/greedy.py
+++ code/algorithms/greedy.py
@@ -6,11 +6,24 @@ class Greedy:
         self.graph = copy.deepcopy(graph)
         self.transmitters = transmitters
 
+    def get_next_node(self, nodes):
+        """
+        Gets the next node with the most neighbours and removes it from the list.
+        """
+        nodes.sort(key=lambda node: len(node.neighbours))
+        return nodes.pop()
+
     def run(self):
         """
         Greedily assigns the lowest costing transmitters to the nodes of the graph.
         """
-        for node in self.graph.nodes.values():
+        nodes = list(self.graph.nodes.values())
+
+        node_possibilities = self.transmitters
+
+        while nodes or not node_possibilities:
+            node = self.get_next_node(nodes)
+
             # Retreive all valid possible values for a node.
             node_possibilities = node.get_possibilities(self.transmitters)
 
--- main.py
+++ main.py
@@ -15,8 +15,8 @@ if __name__ == '__main__':
     transmitters = transmitters.CostScheme('data/transmitters.csv')
 
     # # Random reassignment
-    random_graph = randomize.random_reassignment(test_graph, transmitters.get_scheme(1))
-    print(f"Value of the configuration after Randomized Assignment: {random_graph.calculate_value()}")
+    # random_graph = randomize.random_reassignment(test_graph, transmitters.get_scheme(1))
+    # print(f"Value of the configuration after Randomized Assignment: {random_graph.calculate_value()}")
 
     # Greedy
     greedy = gr.Greedy(test_graph, transmitters.get_scheme((1)))
@@ -25,18 +25,18 @@ if __name__ == '__main__':
     print(f"Value of the configuration after Greedy: {greedy.graph.calculate_value()}")
 
     # Depth First
-    depth = df.DepthFirst(test_graph, transmitters.get_scheme(1)[0:4])
-    depth.run()
-
-    print(f"Value of the configuration after DepthFirst: {depth.graph.calculate_value()}")
-
-    # Hill Climber
-    print("Setting up hillclimber...")
-    climber = hc.HillClimber(greedy.graph, transmitters.get_scheme(1))
-
-    print("Running hillclimber...")
-    climber.run(2000)
-
-    # Visualisation
-    print(f"Value of the configuration after HillClimber: {climber.graph.calculate_value()}")
-    vis.visualise(climber.graph, "data/US/gz_2010_us_040_00_500k.json")
+    # depth = df.DepthFirst(test_graph, transmitters.get_scheme(1)[0:4])
+    # depth.run()
+    #
+    # print(f"Value of the configuration after DepthFirst: {depth.graph.calculate_value()}")
+    #
+    # # Hill Climber
+    # print("Setting up hillclimber...")
+    # climber = hc.HillClimber(greedy.graph, transmitters.get_scheme(1))
+    #
+    # print("Running hillclimber...")
+    # climber.run(2000)
+    #
+    # # Visualisation
+    # print(f"Value of the configuration after HillClimber: {climber.graph.calculate_value()}")
+    # vis.visualise(climber.graph, "data/US/gz_2010_us_040_00_500k.json")
